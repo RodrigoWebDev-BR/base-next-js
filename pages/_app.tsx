2ce2b6c76c83e10693e08082f7eed8a57c73d4f8
Rodrigo <54447826+RodrigoWebDev-BR@users.noreply.github.com>
2022-06-10 10:37:15 -0300

    Deploy 10.06

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,46 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const URL_API = "https://fakestoreapi.com/products/"
+
+export default function Home() {
+
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState(null)
+
+  const fetchAllData = async () => {
+    try {
+      setLoading(true)
+
+      const response = await fetch(URL_API)
+      const data = await response.json()
+
+      if(!data) throw "ERRO NA RIQUISIÇÃO..."
+
+      console.log(response.status, data)
+
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+    
+  }
+
+  useEffect(() => {
+      fetchAllData()
+  }, [])
+
+  return (
+    <div className={styles.container}>
+      {loading && !data && <h2>Carregando...</h2>}
+      {data && data.map((products) => 
+          (<p key={products.id}>{products.title}</p>
+
+          ))}
+    </div>
+  )
+
 }
-
-export default MyApp
